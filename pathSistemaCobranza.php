a480961d8f55cb446080ba6ad33e486a3e9efbd9
LauraSO <lauraferreri@live.com.ar>
2019-08-16 16:31:00 -0300

    Initial commit

---
+++ pathSistemaCobranza.php
@@ -0,0 +1,10 @@
+<?php 
+define $unidad="Z:";
+define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'].'/cobranzasCiberWifi/');
+define('CONTROLLER_PATH', ROOT_PATH.'src/controlador/');
+define('MODEL_PATH', ROOT_PATH.'src/modelo/');
+define('VIEW_PATH', ROOT_PATH.'src/vista/');
+define('CONFIG_PATH', ROOT_PATH.'config/');
+define('API_PATH', ROOT_PATH.'api/');
+
+?>
